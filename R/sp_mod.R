372551fc7c0528a5e7af17f8b790218175830689
Marlon E. Cobos <manubio13@gmail.com>
2018-04-26 11:26:28 -0500

    Correcting typos in documentation

--- R/sp_mod.R
+++ R/sp_mod.R
@@ -1,7 +1,7 @@
 #' An ecological niche model created with Maxent.
 #'
 #' A raster layer containing an ecological niche model for the Turkey tick that was
-#' created as part of the candidate models for teh calibration process. 
+#' created as part of the candidate models during the calibration process.
 #'
 #' @format A RasterLtack with 150 rows, 249 columns, and 37350 cells:
 #' \describe{
