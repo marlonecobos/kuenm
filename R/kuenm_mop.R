6f9f6981381af5fa95a1d07fbe23b39df595611d
Marlon E. Cobos <manubio13@gmail.com>
2018-07-19 13:05:15 -0500

    V 1.0.5 mop previous version working better

--- R/kuenm_mop.R
+++ R/kuenm_mop.R
@@ -8,25 +8,25 @@
 #' @param G.stack a RasterStack of variables representing the full area of interest, and areas
 #' or scenarios to which models are transferred.
 #' @param percent (numeric) percent of values sampled from te calibration region to calculate the MOP.
-#' @param comp.each (numeric) compute distance matrix for a each fixed number of rows (default 1000).
+#' @param normalized (logical) if true values of similarity are presented from 0 to 1,
+#' default = TRUE.
 #'
-#' @return A mobility-oriented parity RasterLayer normalized where values of 0 represent
-#' strict extrapolation.
+#' @return A mobility-oriented parity RasterLayer.
 #'
 #' @details The MOP is calculated following Owens et al.
 #' (2013; \url{https://doi.org/10.1016/j.ecolmodel.2013.04.011}). This function is a modification
 #' of the \code{\link[ENMGadgets]{MOP}} funcion, available at \url{https://github.com/narayanibarve/ENMGadgets}.
 #'
-#'
 #' @examples
 #' mvars <- mvars_mop
 #' gvars <- gvars_mop
 #' perc <- 10
+#' norm <- TRUE
 #'
 #' mop <- kuenm_mop(M.stack = mvars, G.stack = gvars,
-#'                   percent = perc)
+#'                   percent = perc, normalized = norm)
 
-kuenm_mop <- function(M.stack, G.stack, percent = 10, comp.each = 1000) {
+kuenm_mop <- function(M.stack, G.stack, percent = 10, normalized = TRUE) {
   mPoints <- raster::rasterToPoints(M.stack)
   gPoints <- raster::rasterToPoints(G.stack)
 
@@ -37,63 +37,39 @@ kuenm_mop <- function(M.stack, G.stack, percent = 10, comp.each = 1000) {
     stop("Stacks must have the same dimensions")
   }
 
-  steps <- seq(1, dim(m2)[1], comp.each)
+  steps <- seq(1, dim(m2)[1], 1000)
   kkk <- c(steps,  dim(m2)[1] + 1)
   out_index <- plot_out(m1, m2)
   long_k <- length(kkk)
-  suppressPackageStartupMessages(library("doParallel"))
-
-  cores <- parallel::detectCores() - 1
-  cl <- parallel::makeCluster(cores)
-  doParallel::registerDoParallel(cl)
 
   mop1 <- lapply(1:(length(kkk) - 1), function(x) {
     seq_rdist <- kkk[x]:(kkk[x + 1] - 1)
     eudist <- fields::rdist(m2[seq_rdist, ], m1)
-    percent <- percent
-    mean_quantile <- foreach::foreach(y = 1:dim(eudist)[1],
-                                      .packages = c("kuenm")) %dopar% {
-                                        mop_dist(eudist.matrix = eudist,
-                                                 irow = y,
-                                                 percent = percent)
+    mean_quantile <- parallel::mclapply(1:dim(eudist)[1], function(y) {
+      di <- eudist[y, ]
+      qdi <- quantile(di, probs = percent / 100, na.rm = TRUE)
+      ii <-  which(di <= qdi)
+      return(mean(di[ii]))
+    })
 
-                                      }
     avance <- (x / long_k) * 100
     cat("Computation progress: ", avance,"%" ,"\n")
 
     return(unlist(mean_quantile))
   })
 
-  parallel::stopCluster(cl)
-
   mop2 <- unlist(mop1)
   mop_all <- data.frame(gPoints[, 1:2], mop2)
   mop_max <- max(na.omit(mop2))
-  mop_max <- max(mop2)
-  mop_all[out_index, 3] <- mop_max * 1.05
+  # What about using -1 instead of NA?
+  mop_all[out_index, 3] <- NA
   sp::coordinates(mop_all) <- ~x + y
   sp::gridded(mop_all) <- TRUE
   mop_raster <- raster::raster(mop_all)
 
-  mop_raster <- 1 - (mop_raster / mop_max)
+  if(normalized) {
+    mop_raster <- 1 - (mop_raster / mop_max)
+  }
 
   return(mop_raster)
-}
-
-#' MOP distance
-#'
-#' @description mop_dist computes a ponderated Euclidean distance for MOP.
-#'
-#' @param eudist.matrix a matrix of Euclidean distance between M and G areas.
-#' @param irow row from eudist_matrix where the pondarated distance will be compute
-#' @param percent (numeric) percent of values sampled from te calibration region to calculate the MOP.
-#' @return a vector of poderated distances for irow
-
-
-mop_dist <-  function(eudist.matrix, irow, percent) {
-  di <- eudist.matrix[irow, ]
-  qdi <- quantile(di, probs = percent / 100, na.rm = TRUE)
-  ii <-  which(di <= qdi)
-
-  return(mean(di[ii]))
 }
